ab4cd3cb6d13a20f1c54e86ab2e71ddf98adef38
hax0kartik <pokemonemerald0@gmail.com>
2020-08-20 16:26:48 +0530

    Convert indentation to spaces

--- source/main.cpp
+++ source/main.cpp
@@ -12,106 +12,106 @@
 
 void debug(nana::textbox *box, std::string str)
 {
-	box->append(str, false);
+   box->append(str, false);
 }
 
 void start(Communicator *comm, nana::form *fm, nana::textbox *box)
 {
-	std::function<void(std::string)> debugfn = std::bind(debug, box, std::placeholders::_1);
+   std::function<void(std::string)> debugfn = std::bind(debug, box, std::placeholders::_1);
 
-	auto error = [](nana::form *fm, std::string message) -> void {
-		nana::msgbox m(*fm, "Error");
-		m.icon(m.icon_error);
-		m << message;
-		auto response = m();
-	};
+   auto error = [](nana::form *fm, std::string message) -> void {
+      nana::msgbox m(*fm, "Error");
+      m.icon(m.icon_error);
+      m << message;
+      auto response = m();
+   };
 
-	comm->SetDebugFunction(debugfn);
-	debugfn("Reading Files.\n");
-	if(comm->ReadFiles() != 0)
-	{
-		error(fm, "An Error occured while reading files");
-		return;
-	}
+   comm->SetDebugFunction(debugfn);
+   debugfn("Reading Files.\n");
+   if(comm->ReadFiles() != 0)
+   {
+      error(fm, "An Error occured while reading files");
+      return;
+   }
 
-	debugfn("Files parsed successfully.\n");
-	if(comm->ParseFiles() != 0)
-	{
-		error(fm, "Files are invalid");
-		return;
-	}
+   debugfn("Files parsed successfully.\n");
+   if(comm->ParseFiles() != 0)
+   {
+      error(fm, "Files are invalid");
+      return;
+   }
 
-	debugfn("Connecting to 3DS.\n");
-	if(comm->ConnectTo3DS() != 0)
-	{
-		error(fm, "Could not connect to 3DS, check IP Address and the internet connection.");
-		return;
-	}
-	debugfn("Connected.\n");
-	comm->IPCServer();
-	comm->FlushToFileIfRequired();
+   debugfn("Connecting to 3DS.\n");
+   if(comm->ConnectTo3DS() != 0)
+   {
+      error(fm, "Could not connect to 3DS, check IP Address and the internet connection.");
+      return;
+   }
+   debugfn("Connected.\n");
+   comm->IPCServer();
+   comm->FlushToFileIfRequired();
 }
 
 int main()
 {
-	using namespace nana;
-	Communicator comm;
-	form fm {API::make_center(300, 400), appearance{true, true, false, false, true, false, false}};
-	nana::filebox picker{nullptr, true};
-	fm.caption("Wummibo");     
+   using namespace nana;
+   Communicator comm;
+   form fm {API::make_center(300, 400), appearance{true, true, false, false, true, false, false}};
+   nana::filebox picker{nullptr, true};
+   fm.caption("Wummibo");     
 
-	label enc_label{fm, rectangle{10, 10, 110, 30}}; // pos x:y:width:height
-	label dec_label{fm, rectangle{10, 40, 110, 30}};
-	label ip_addr{fm, rectangle{10, 70, 110, 30}};
-	enc_label.caption("Enc amiibo file:");
-	dec_label.caption("Dec amiibo file:");
-	ip_addr  .caption("3DS IP Address:");
-		 
-	textbox enc_loc {fm, rectangle{100, 4, 150, 25}, true};
-	textbox dec_loc {fm, rectangle{100, 34, 150, 25}, true};
-	textbox ip_loc  {fm, rectangle{100, 64, 150, 25}, true};
-	textbox debug_loc {fm, rectangle{10, 160, 280, 200}, true};
-	enc_loc.editable(false);
-	dec_loc.editable(false);
-	debug_loc.editable(false);
+   label enc_label{fm, rectangle{10, 10, 110, 30}}; // pos x:y:width:height
+   label dec_label{fm, rectangle{10, 40, 110, 30}};
+   label ip_addr{fm, rectangle{10, 70, 110, 30}};
+   enc_label.caption("Enc amiibo file:");
+   dec_label.caption("Dec amiibo file:");
+   ip_addr  .caption("3DS IP Address:");
+       
+   textbox enc_loc {fm, rectangle{100, 4, 150, 25}, true};
+   textbox dec_loc {fm, rectangle{100, 34, 150, 25}, true};
+   textbox ip_loc  {fm, rectangle{100, 64, 150, 25}, true};
+   textbox debug_loc {fm, rectangle{10, 160, 280, 200}, true};
+   enc_loc.editable(false);
+   dec_loc.editable(false);
+   debug_loc.editable(false);
 
-	button enc_button {fm, rectangle{260, 4, 30, 25}};
-	enc_button.caption("...");
-	button dec_button {fm, rectangle{260, 34, 30, 25}};
-	dec_button.caption("...");
-	button set_ip {fm, rectangle{260, 64, 30, 25}};
-	set_ip.caption("set");
-		
-	button emulate {fm, rectangle {110, 120, 90, 30}};
-	emulate.caption("Emulate");
-		 
-	enc_button.events().click([&]() {
-							auto paths = picker.show();
-							if(!paths.empty()){
-									enc_loc.append(paths[0].filename(), true);
-									comm.SetEncryptedFile(paths[0].u8string());
-							}
-						});
+   button enc_button {fm, rectangle{260, 4, 30, 25}};
+   enc_button.caption("...");
+   button dec_button {fm, rectangle{260, 34, 30, 25}};
+   dec_button.caption("...");
+   button set_ip {fm, rectangle{260, 64, 30, 25}};
+   set_ip.caption("set");
+      
+   button emulate {fm, rectangle {110, 120, 90, 30}};
+   emulate.caption("Emulate");
+       
+   enc_button.events().click([&]() {
+                     auto paths = picker.show();
+                     if(!paths.empty()){
+                           enc_loc.append(paths[0].filename(), true);
+                           comm.SetEncryptedFile(paths[0].u8string());
+                     }
+                  });
 
-	dec_button.events().click([&]() {
-							auto paths = picker.show();
-							if(!paths.empty()){
-									dec_loc.append(paths[0].filename(), true);
-									comm.SetDecryptedFile(paths[0].u8string());
-							}
-						});
+   dec_button.events().click([&]() {
+                     auto paths = picker.show();
+                     if(!paths.empty()){
+                           dec_loc.append(paths[0].filename(), true);
+                           comm.SetDecryptedFile(paths[0].u8string());
+                     }
+                  });
 
-	set_ip.events().click([&]() {
-							std::string ip;
-							if(ip_loc.getline(0, ip))
-								comm.SetIPAddr(ip);
-		 });
+   set_ip.events().click([&]() {
+                     std::string ip;
+                     if(ip_loc.getline(0, ip))
+                        comm.SetIPAddr(ip);
+       });
 
-	emulate.events().click([&]() {
-							std::thread(start, &comm, &fm, &debug_loc).detach();
-		 	});
+   emulate.events().click([&]() {
+                     std::thread(start, &comm, &fm, &debug_loc).detach();
+          });
 
-	// fm_place.collocate();                      // and collocate all in place
-	fm.show();
-	exec();
+   // fm_place.collocate();                      // and collocate all in place
+   fm.show();
+   exec();
  }
